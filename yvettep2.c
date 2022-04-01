336ee62d40353e122d5d8cbfa2aeb2a6890919c1
Yvette Green <green.y@northeastern.edu>
2022-04-01 11:15:47 -0700

    Update yvettep2.c

--- yvettep2.c
+++ yvettep2.c
@@ -1,1 +1,35 @@
+/*
+Stairs crossing problem using pThreads and Semaphores
+*/
 
+#include <pthread.h>  
+#include <semaphore.h> 
+#include <stdio.h>    
+#include <stdlib.h>
+#include <unistd.h>	
+
+
+#define MAX_CUSTOMERS 1
+#define maxStairs 13
+
+
+typedef enum direction 
+{
+    DIRECTION_UP = 0;
+    DIRECTION_DOWN,
+    DIRECTION_MAX
+}enumDirection_t;
+
+typedef enum error
+{
+    ERROR = -1,
+    SUCCESS,
+    ERROR_MAX
+}error_t;
+
+typedef struct customer
+{
+    enumDirection_t directionFlag;
+    int sleep;
+
+}
