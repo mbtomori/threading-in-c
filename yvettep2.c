7e6707977929ad7e752b0798807a253eb3fc7164
mbtomori <mbtomori@users.noreply.github.com>
2022-04-04 21:17:30 -0700

    Update variable and function names

--- yvettep2.c
+++ yvettep2.c
@@ -3,342 +3,338 @@
 Stairs crossing problem using pThreads and Semaphores
 */
 
-#include <pthread.h>  
-#include <semaphore.h> 
-#include <stdio.h>    
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>	
+#include <unistd.h>
 
-#ifndef tNum
+#ifndef MAX_STORE_CUSTOMERS 
 // TODO
-// #define tNum: which is the maximum number of customers/threads in the system to test
-#define tNum 10
-// #define maxStairs: how many customer can be on the stairs at the same time
-#define maxStairs 13
+// #define MAX_STORE_CUSTOMERS : which is the maximum number of customers/threads in the system to test
+#define MAX_STORE_CUSTOMERS  10
+// #define MAX_STAIR_USERS: how many customer can be on the stairs at the same time
+#define MAX_STAIR_USERS 13
 // you can also define other constants for your "prevent deadlock" or "prevent starvation" algorithm
 #endif 
 
 
 //TODO 
 // create a struct as Thread's data. Contains the thread's ID.
-typedef struct _tData_t	{int tID; }tData_t;
+typedef struct _customer_t
+{
+    int tID; 
+} customer_t;
  
 // TODO
 // define semaphores you will use
-sem_t goingUp, goingDown, mutex;
+sem_t ascend;
+sem_t descend;
+sem_t stair_lock;
 
 /* 
- *	the following variables are created to:  
- *	1. prevent deadlock
- *	2. prevent starvation
- *	3. allow more than one customer to use the stairs in the same direction in an “efficient” way
+ *  the following variables are created to:  
+ *  1. prevent deadlock
+ *  2. prevent starvation
+ *  3. allow more than one customer to use the stairs in the same direction in an “efficient” way
        that you determine
  */
-//int xingCnt, xedCnt, toAwaitCnt, toBwaitCnt; 
-int xingCnt, xedCnt, toUpwaitCnt, toDownwaitCnt; 
+
+int current_stair_users, num_users_crossed, number_waiting_to_ascend, num_waiting_to_descend; 
 
 
 /* 
- *	flag the crossing direction (0 noDir, 1 upDir, 2 downDir)
  * I used an enum instead of int 
  */
-//int xingDir; 
-enum {noDir, upDir, downDir} xingDir; 
+enum {none, up, down} stair_direction; 
 
 
-//Customers going toward A (i.e. up) or toward B (i.e. down) respectively
-//void *a(void *arg);
-//void *b(void *arg);
-//I used up and down instead
-void *up(void *arg);
-void *down(void *arg);
+//Functions to "use the stairs"
+void *ascend_stairs(void *arg);
+void *descend_stairs(void *arg);
 
 //Semaphore wait and signal functions
-void semWait(sem_t *sem);
-void semSignal(sem_t *sem);
+void semaphore_wait(sem_t *sem);
+void semaphore_signal(sem_t *sem);
 
 
 int main(){
 
  
-	printf("Project 2: Customer crossing problem using pThreads and Semaphores\n");
-	printf("*****************\n\n");
-	fflush(stdout);
-
-	// TODO
-	/*
-	 *	initialize your pthread, semaphore and other variables you will need
-	 *	please do error check if the program fail to pthread_create()
-	 */
-
-	tData_t tData[tNum];
-	pthread_t thread[tNum];
-
-	int errCheck;
-
-	if (sem_init(&mutex, 0, (unsigned int)1) < 0
-        || sem_init(&goingUp, 0, (unsigned int)1) < 0
-        || sem_init(&goingDown, 0, (unsigned int)1) < 0) {
-        perror("sem_init");
-        exit(EXIT_FAILURE);
-	}
-
-    printf("Parent Process PID: %d \n", getpid());//parent PID
-	fflush(stdout);
-	
+    printf("Project 2: Customer crossing problem using pThreads and Semaphores\n");
+    printf("*****************\n\n");
+    fflush(stdout);
 
     // TODO
-	/*
-	 *	do pthread_create() and call your upstair/downstair func
-	 */
-	 for (int i = 0; i < tNum; ++i) {
+    /*
+     *  initialize your pthread, semaphore and other variables you will need
+     *  please do error check if the program fail to pthread_create()
+     */
 
-		void *thread_func;
+    customer_t customer_data[MAX_STORE_CUSTOMERS ];
+    pthread_t thread[MAX_STORE_CUSTOMERS ];
 
-		tData[i].tID = i;
+    int errCheck;
 
-		if (rand()%tNum <=tNum/2) {
-			thread_func = up;
-		} 
-		else{
-			thread_func = down;
-		}
+    if (sem_init(&stair_lock, 0, (unsigned int)1) < 0
+        || sem_init(&ascend, 0, (unsigned int)1) < 0
+        || sem_init(&descend, 0, (unsigned int)1) < 0) {
+        perror("sem_init");
+        exit(EXIT_FAILURE);
+    }
 
-		sleep(1);
-		fflush(stdout);
-		sleep(1);
-		fflush(stdout);
+    printf("Parent Process PID: %d \n", getpid());//parent PID
+    fflush(stdout);
+    
 
-	if ((errCheck = pthread_create(&thread[i], NULL, thread_func, &tData[i]))) {
+    // TODO
+     for (int i = 0; i < MAX_STORE_CUSTOMERS ; ++i) {
+
+        void *thread_func;
+
+        customer_data[i].tID = i;
+
+        if (rand()%MAX_STORE_CUSTOMERS  <=MAX_STORE_CUSTOMERS /2) {
+            thread_func = ascend_stairs;
+        } 
+        else{
+            thread_func = descend_stairs;
+        }
+
+        // sleep(1);
+        // fflush(stdout);
+        sleep(1);
+        fflush(stdout);
+
+    if ((errCheck = pthread_create(&thread[i], NULL, thread_func, &customer_data[i]))) {
             fprintf(stderr, "error: pthread_create, %d\n", errCheck);
             return EXIT_FAILURE;
         }
     }
     
-    for (int i = 0; i < tNum; ++i) {
+    for (int i = 0; i < MAX_STORE_CUSTOMERS ; ++i) {
         if ((errCheck = pthread_join(thread[i], NULL))) {
             fprintf(stderr, "error: pthread_join, %d\n", errCheck);
         }
     }
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 /*
-* Function for the customer to B level
-* down function
+* Function for the customer to descend to lower level
 */
-void *down(void *arg){
+void *descend_stairs(void *arg){
 
 
-	// TODO
-	/*
-	 *	process the toB action - down function
-	 *	print logs to show customers status (e.g., "customer is Crossing from A to B", 
-	 *  "customer to B should wait", “Finished Stairs") 
-	 */
-	tData_t *data = (tData_t *)arg;
+    // TODO
+    /*
+     *  process the toB action - down function
+     *  print logs to show customers status (e.g., "customer is Crossing from A to B", 
+     *  "customer to B should wait", “Finished Stairs") 
+     */
+    customer_t *data = (customer_t *)arg;
 
-	int tIDdown = data->tID;
-	semWait(&mutex);
+    int tIDdown = data->tID;
+    semaphore_wait(&stair_lock);
 
-	printf("****\nThread %d: customer would like to cross from A to B, going down\n", tIDdown);
-	fflush(stdout);
+    printf("****\nThread %d: customer would like to cross from A to B, going down\n", tIDdown);
+    fflush(stdout);
 
-	if ((xingDir == downDir || xingDir == noDir) && 
-		(xingCnt < 5) &&
-		((xedCnt + xingCnt) < 10)){
+    if ((stair_direction == down || stair_direction == none) && 
+        (current_stair_users < 5) &&
+        ((num_users_crossed + current_stair_users) < 10)){
 
-		printf("Customer is Crossing from A to B, going down\n");
-		fflush(stdout);
+        printf("Customer is Crossing from A to B, going down\n");
+        fflush(stdout);
 
-		xingDir = downDir;
-		xingCnt++;
+        stair_direction = down;
+        current_stair_users++;
 
-		printf("Signaling Mutex\n");
-		fflush(stdout);
+        printf("Signaling stair_lock\n");
+        fflush(stdout);
 
-		semSignal(&mutex);
-	}
-	else{
-		
-		printf("Customer is waiting to go down\n");
-		fflush(stdout);
-		toDownwaitCnt++;
+        semaphore_signal(&stair_lock);
+    }
+    else{
+        
+        printf("Customer is waiting to go down\n");
+        fflush(stdout);
+        num_waiting_to_descend++;
 
-		printf("Signaling Mutex\n");
-		fflush(stdout);
-		semWait(&mutex);
-		semWait(&goingDown);
+        printf("Signaling stair_lock\n");
+        fflush(stdout);
+        semaphore_wait(&stair_lock);
+        semaphore_wait(&descend);
 
-		toDownwaitCnt--;
-		xingCnt++;
-		xingDir = downDir;
+        num_waiting_to_descend--;
+        current_stair_users++;
+        stair_direction = down;
 
-		semSignal(&mutex);
-	}
+        semaphore_signal(&stair_lock);
+    }
 
-	printf ("Using Stairs\n");
-	printf("Finished Stairs. Waiting for mutex.\n");
-	fflush(stdout);
-	semWait(&mutex);
-	
-	printf("Mutex Passed\n");
-	fflush(stdout);
-	xedCnt++;
-	xingCnt--;
+    printf ("Using Stairs\n");
+    printf("Finished Stairs. Waiting for stair_lock.\n");
+    fflush(stdout);
+    semaphore_wait(&stair_lock);
+    
+    printf("stair_lock Passed\n");
+    fflush(stdout);
+    num_users_crossed++;
+    current_stair_users--;
 
-	if (toDownwaitCnt!= 0 &&
-		(xingCnt + xedCnt < 10 ||
-			(xedCnt + xingCnt>= 10 && toUpwaitCnt == 0))){
+    if (num_waiting_to_descend!= 0 &&
+        (current_stair_users + num_users_crossed < 10 ||
+            (num_users_crossed + current_stair_users>= 10 && number_waiting_to_ascend == 0))){
 
-		semSignal(&goingDown);
-	}
+        semaphore_signal(&descend);
+    }
 
-	else if(xingCnt == 0 &&
-		toUpwaitCnt !=0 &&
-		(toDownwaitCnt == 0 ||
-			xedCnt + toDownwaitCnt >= 10)){
+    else if(current_stair_users == 0 &&
+        number_waiting_to_ascend !=0 &&
+        (num_waiting_to_descend == 0 ||
+            num_users_crossed + num_waiting_to_descend >= 10)){
 
-		xingDir = upDir;
-		xedCnt = 0;
-		semSignal(&goingUp);
-	}
+        stair_direction = up;
+        num_users_crossed = 0;
+        semaphore_signal(&ascend);
+    }
 
-	else if(xingCnt == 0 &&
-		toDownwaitCnt == 0 && 
-		toUpwaitCnt == 0){
-		xingDir = noDir;
-		xedCnt = 0;
+    else if(current_stair_users == 0 &&
+        num_waiting_to_descend == 0 && 
+        number_waiting_to_ascend == 0){
+        stair_direction = none;
+        num_users_crossed = 0;
 
-		printf("Crossing Direction Reset \n");
-		fflush(stdout);
-		semSignal(&mutex);
-	}
+        printf("Crossing Direction Reset \n");
+        fflush(stdout);
+        semaphore_signal(&stair_lock);
+    }
 
-	else{
-		printf("Signaling Mutex\n");
-		fflush(stdout);
-		semSignal(&mutex);
-	}
+    else{
+        printf("Signaling stair_lock\n");
+        fflush(stdout);
+        semaphore_signal(&stair_lock);
+    }
 
-	pthread_exit(NULL);
+    pthread_exit(NULL);
 }
 
 
 /*
 * Function for the customer to A level
 */
-void *up(void *arg){
+void *ascend_stairs(void *arg){
 
 
-	// TODO
-	/*
-	 *	process the toA action. 
-	 *	print logs to show customers status (e.g., "customer is Crossing from B to A", 
-	 *  "customer to A should wait", “Finished Stairs") 
-	 */
+    // TODO
+    /*
+     *  process the toA action. 
+     *  print logs to show customers status (e.g., "customer is Crossing from B to A", 
+     *  "customer to A should wait", “Finished Stairs") 
+     */
 
-	tData_t *data = (tData_t *)arg;
+    customer_t *data = (customer_t *)arg;
 
-	int tIDup = data->tID;
-	semWait(&mutex);
+    int tIDup = data->tID;
+    semaphore_wait(&stair_lock);
 
-	printf("****\nThread %d: customer would like to cross from B to A, going up\n", tIDup);
-	fflush(stdout);
+    printf("****\nThread %d: customer would like to cross from B to A, going up\n", tIDup);
+    fflush(stdout);
 
-	if ((xingDir == upDir || xingDir == noDir) && 
-		xingCnt < 5 &&
-		(xedCnt + xingCnt < 10)){
+    if ((stair_direction == up || stair_direction == none) && 
+        current_stair_users < 5 &&
+        (num_users_crossed + current_stair_users < 10)){
 
-		printf("Customer is Crossing from B to A, going up\n");
-		fflush(stdout);
+        printf("Customer is Crossing from B to A, going up\n");
+        fflush(stdout);
 
-		xingDir = upDir;
-		xingCnt++;
+        stair_direction = up;
+        current_stair_users++;
 
-		printf("Signaling Mutex\n");
-		fflush(stdout);
+        printf("Signaling stair_lock\n");
+        fflush(stdout);
 
-		semSignal(&mutex);
-	}
-	else{
-		printf("A is now supposed to wait to go up\n");
-		fflush(stdout);
-		toUpwaitCnt++;
+        semaphore_signal(&stair_lock);
+    }
+    else{
+        printf("A is now supposed to wait to go up\n");
+        fflush(stdout);
+        number_waiting_to_ascend++;
 
-		printf("Signaling mutex\n");
-		fflush(stdout);
-		semWait(&mutex);
-		semWait(&goingUp);
+        printf("Signaling stair_lock\n");
+        fflush(stdout);
+        semaphore_wait(&stair_lock);
+        semaphore_wait(&ascend);
 
-		toUpwaitCnt--;
-		xingCnt++;
-		xingDir = upDir;
+        number_waiting_to_ascend--;
+        current_stair_users++;
+        stair_direction = up;
 
-		printf("Customer about to go up\n");
-		fflush(stdout);
-		printf("Signaling Mutex\n");
-		semSignal(&mutex);
-	}
-
-
-	printf ("Using Stairs\n");
-	printf("Finished Stairs. Waiting for mutex \n");
-	fflush(stdout);
-	semWait(&mutex);
-
-	printf("Mutex Passed\n");
-	fflush(stdout);
-	xedCnt++;
-	xingCnt--;
-
-	
-	if (toUpwaitCnt!= 0 &&
-		(xingCnt + xedCnt < 10 ||
-			(xedCnt + xingCnt>= 10 && toDownwaitCnt == 0))){
-
-		semSignal(&goingUp);
-	}
-
-	else if(xingCnt == 0 &&
-		toDownwaitCnt !=0 &&
-		(toUpwaitCnt == 0 ||
-			xedCnt + toDownwaitCnt >= 10)){
-		
-		xingDir = downDir;
-		xedCnt = 0;
-		semSignal(&goingDown);
-	}
-
-	else if(xingCnt == 0 &&
-		toUpwaitCnt == 0 && 
-		toDownwaitCnt == 0){
-		printf("Crossing Direction Reset \n");
-		fflush(stdout);
-		xingDir = noDir;
-		xedCnt = 0;
-
-		printf("Signaling Mutex\n");
-		fflush(stdout);
-		semSignal(&mutex);
-	}
-
-	else{
-		printf("Signaling Mutex\n");
-		fflush(stdout);
-		semSignal(&mutex);
-	}
+        printf("Customer about to go up\n");
+        fflush(stdout);
+        printf("Signaling stair_lock\n");
+        semaphore_signal(&stair_lock);
+    }
 
 
-	pthread_exit(NULL);
+    printf ("Using Stairs\n");
+    printf("Finished Stairs. Waiting for stair_lock \n");
+    fflush(stdout);
+    semaphore_wait(&stair_lock);
+
+    printf("stair_lock Passed\n");
+    fflush(stdout);
+    num_users_crossed++;
+    current_stair_users--;
+
+    
+    if (number_waiting_to_ascend!= 0 &&
+        (current_stair_users + num_users_crossed < 10 ||
+            (num_users_crossed + current_stair_users>= 10 && num_waiting_to_descend == 0))){
+
+        semaphore_signal(&ascend);
+    }
+
+    else if(current_stair_users == 0 &&
+        num_waiting_to_descend !=0 &&
+        (number_waiting_to_ascend == 0 ||
+            num_users_crossed + num_waiting_to_descend >= 10)){
+        
+        stair_direction = down;
+        num_users_crossed = 0;
+        semaphore_signal(&descend);
+    }
+
+    else if(current_stair_users == 0 &&
+        number_waiting_to_ascend == 0 && 
+        num_waiting_to_descend == 0){
+        printf("Crossing Direction Reset \n");
+        fflush(stdout);
+        stair_direction = none;
+        num_users_crossed = 0;
+
+        printf("Signaling stair_lock\n");
+        fflush(stdout);
+        semaphore_signal(&stair_lock);
+    }
+
+    else{
+        printf("Signaling stair_lock\n");
+        fflush(stdout);
+        semaphore_signal(&stair_lock);
+    }
+
+
+    pthread_exit(NULL);
 }
 
 // helper functions
 /*
- *	Error-checked semaphore wait.
+ *  Error-checked semaphore wait.
  */
-void semWait(sem_t *sem) {
+void semaphore_wait(sem_t *sem) {
     if (sem_wait(sem) < 0) {
         perror("sem_wait");
         exit(EXIT_FAILURE);
@@ -346,9 +342,9 @@ void semWait(sem_t *sem) {
 }
 
 /*
- *	Error-checked semaphore signal.
+ *  Error-checked semaphore signal.
  */
-void semSignal(sem_t *sem) {
+void semaphore_signal(sem_t *sem) {
     if (sem_post(sem) < 0) {
         perror("sem_post");
         exit(EXIT_FAILURE);
@@ -356,7 +352,7 @@ void semSignal(sem_t *sem) {
 }
 
 /*
- *	Expected test result:
+ *  Expected test result:
  * Please print logs to show the customers' status for testing. Here is an example:
 Project 2: Customer crossing problem using pThreads and Semaphores
 *****************
@@ -365,31 +361,31 @@ Parent Process PID: 86672
 Thread 0: About to cross
 customer is Crossing from B to A
 Crossing Finished
-Signaling Mutex
+Signaling stair_lock
 Using Stairs 
-Finished Stairs . Waiting for mutex 
-Mutex Passed
+Finished Stairs . Waiting for stair_lock 
+stair_lock Passed
 Crossing Direction Reset 
-Signaling Mutex
+Signaling stair_lock
 ****
 Thread 1: About to cross
 customer is Crossing from B to A
 Crossing Finished
-Signaling Mutex
+Signaling stair_lock
 Using Stairs 
-Finished Stairs . Waiting for mutex 
-Mutex Passed
+Finished Stairs . Waiting for stair_lock 
+stair_lock Passed
 Crossing Direction Reset 
-Signaling Mutex
+Signaling stair_lock
 ****
 Thread 2: customer Crossing from A to B
 customer is Crossing from A to B
-Signaling Mutex
+Signaling stair_lock
 There are 0 customers waiting 
 Using Stairs 
 The customer will finish crossing in 1 seconds 
-Finished Stairs. Waiting for mutex 
-Mutex Passed
+Finished Stairs. Waiting for stair_lock 
+stair_lock Passed
 Crossing Direction Reset 
 ****
 
