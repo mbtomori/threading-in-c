ce91508531d11ea3c6481cca8f095eec0ad5ec22
mbtomori <mbtomori@users.noreply.github.com>
2022-04-06 16:26:11 -0700

    Add documentationand comments

--- stairs.c
+++ stairs.c
@@ -9,47 +9,47 @@ Stairs crossing problem using pThreads and Semaphores
 #include <unistd.h>
 #include <time.h>
 
-#ifndef MAX_STORE_CUSTOMERS
-// TODO
-// #define MAX_STORE_CUSTOMERS : which is the maximum number of customers/threads in the system to test
-#define MAX_STORE_CUSTOMERS 10
-// #define MAX_STAIR_USERS: how many customer can be on the stairs at the same time
-#define MAX_STAIR_USERS 4
-// you can also define other constants for your "prevent deadlock" or "prevent starvation" algorithm
-#endif 
+// DEFINE CONSTANTS
+#define MAX_STORE_CUSTOMERS 10 // max customers in the store
+#define MAX_STAIR_USERS 4  // max customers who can use the stairs simultaneously
 
 
-//TODO 
-// create a struct as the cusomer's data. Contains the customer's ID.
+/*  **STRUCTS **
+ This struct defines a customer */
 typedef struct customer_t
 {
     int id;
 } customer_t;
 
+// This struct creates the most basic of lightswitches
+// that contains a counter (to determine if someone is
+// the first to the stairs)
 typedef struct lightswitch_t
 {
     int counter;
 } lightswitch_t;
 
-
+// ** SEMAPHORE DEFININTION
 // counting semaphores
+// These limit the number of people who can use the stairs simultaneously
 sem_t first_floor_gatekeeper;
 sem_t second_floor_gatekeeper;
 
-// direction semaphores
+// These semaphores protect the direction the stair is currently supporting
 sem_t up_direction;
 sem_t down_direction;
 
-//initialize the lightswitches
+//These are the semaphores used in the lightswitches
 lightswitch_t up_switch;
 lightswitch_t down_switch;
 
-// determine if you're the first person on stairs
+// Tracks whether the stairway is currently empty
 sem_t empty_stairs;
 
-// binary turnstile semaphore to avoid starvation
+// Semaphore that acts as the turnstile to prevent starvation
 sem_t stair_manager;
 
+// FUNCTION DEFINITIONS
 //Functions to "use the stairs"
 void *ascend_stairs(void *arg);
 void *descend_stairs(void *arg);
@@ -65,8 +65,10 @@ void reset_direction(char *direction);
 void switch_lock(lightswitch_t *direction, sem_t *direction_semaphore, sem_t *locking_semaphore);
 void switch_unlock(lightswitch_t *direction, sem_t *direction_semaphore, sem_t *locking_semaphore);
 
-
+// Helper function to calculate the Turnaround Time and Response Time for each thread
 void print_clock_array(void);
+
+// Arrays to track the times threads were created, started, and completed
 clock_t thread_create_time[MAX_STORE_CUSTOMERS];
 clock_t thread_start_time[MAX_STORE_CUSTOMERS];
 clock_t thread_complete_time[MAX_STORE_CUSTOMERS];
@@ -78,16 +80,17 @@ int main(void)
     printf("*****************\n\n");
     fflush(stdout);
 
+    // Arrays to track customer ID and threads
     customer_t customer_data[MAX_STORE_CUSTOMERS];
     pthread_t store_customers[MAX_STORE_CUSTOMERS];
 
-    // set values of switches
+    // set initial value of switch counters to 0.
     up_switch.counter = 0;
     down_switch.counter = 0;
 
     int errCheck;
 
-    // initialize the semaphores and check they were initialized properly
+    // Initialize the semaphores and check they were initialized properly
     if (sem_init(&first_floor_gatekeeper, 0, (unsigned int)MAX_STAIR_USERS) < 0
         || sem_init(&second_floor_gatekeeper, 0, (unsigned int)MAX_STAIR_USERS) < 0
         || sem_init(&stair_manager, 0, (unsigned int)1) < 0
@@ -101,11 +104,10 @@ int main(void)
 
     printf("Parent Process PID: %d \n", getpid()); //parent PID
     fflush(stdout);
-    
 
-    // TODO
-     for (int i = 0; i < MAX_STORE_CUSTOMERS ; ++i)
-     {
+    // Create the threads
+    for (int i = 0; i < MAX_STORE_CUSTOMERS ; ++i)
+    {
 
         void *thread_func;
 
@@ -128,11 +130,9 @@ int main(void)
 
         // set creation start_time
         thread_create_time[i] = clock();
-
     }
 
-
-    
+    // Join the threads
     for (int i = 0; i < MAX_STORE_CUSTOMERS ; ++i)
     {
         if ((errCheck = pthread_join(store_customers[i], NULL)))
@@ -141,8 +141,8 @@ int main(void)
         }
     }
 
+    // Print the Turnaround Time and Response Time for each thread.
     print_clock_array();
-
 
     // destroy the semaphores to prevent memory leaks
     sem_destroy(&second_floor_gatekeeper);
@@ -161,7 +161,6 @@ int main(void)
 void *descend_stairs(void *customer_thread)
 {
     customer_t *data = (customer_t *)customer_thread;
-
     int customer_id = data->id;
 
     // set process start time
@@ -172,28 +171,33 @@ void *descend_stairs(void *customer_thread)
     // Check stair_manager lock to see if there are others on the stairs.
     semaphore_wait(&stair_manager);
     printf("DESC: Customer %d checking stair_manager lock.\n", customer_id);
+    // Check if the direction can/should be set.
     switch_lock(&down_switch, &down_direction, &empty_stairs);
+    // Release the stair manager to let the customer enter the stairs
     semaphore_signal(&stair_manager);
+    // Call the counting semaphore to use the stairs
     semaphore_wait(&second_floor_gatekeeper);
-
+    // Use the stairs
     printf("**DESC: Customer %d is descending.\n", customer_id);
     sleep(1);
     printf("***DESC: Customer %d reached the first floor.\n", customer_id);
+    // Release the counting semaphore to decrement the number of customers on stairs
     semaphore_signal(&second_floor_gatekeeper);
+    // Reset the stair direction if alone on the stairs
     switch_unlock(&down_switch, &down_direction, &empty_stairs);
-    // set process completion time
+    // Set process completion time
     thread_complete_time[customer_id] = clock();
     pthread_exit(NULL);
 }
 
 
 /*
-* Function for the customer to 2nd level
+* Function to enable customer to ascend to the second floor. 
+* This uses the reverse logic of the descend stair function.
 */
 void *ascend_stairs(void *customer_thread)
 {
     customer_t *data = (customer_t *)customer_thread;
-
     int customer_id = data->id;
 
     // set process start time
@@ -203,14 +207,19 @@ void *ascend_stairs(void *customer_thread)
 
     // Check stair_manager lock to see if there are others on the stairs.
     semaphore_wait(&stair_manager);
+    // Set the direction if customer is first to the stairs
     switch_lock(&up_switch, &up_direction, &empty_stairs);
+    // Release the stair manager to let a customer enter staircase
     semaphore_signal(&stair_manager);
+    // Release counting semaphore to decrement the number of customers using the stairs.
     semaphore_wait(&first_floor_gatekeeper);
-
+    // Use the stairs
     printf("**ASC: Customer %d is ascending.\n", customer_id);
     sleep(1);
     printf("***ASC: Customer %d reached the second floor.\n", customer_id);
+    // Decrement the number of people using the stairs
     semaphore_signal(&first_floor_gatekeeper);
+    // Reset direction if the only person on stairs
     switch_unlock(&up_switch, &up_direction, &empty_stairs);
 
     // set process completion time
@@ -219,9 +228,9 @@ void *ascend_stairs(void *customer_thread)
     pthread_exit(NULL);
 }
 
-// helper functions
+// Helper functions
 /*
- *  Error-checked semaphore wait.
+ *  Error-checked semaphore wait from provided skeleton
  */
 void semaphore_wait(sem_t *sem)
 {
@@ -233,7 +242,7 @@ void semaphore_wait(sem_t *sem)
 }
 
 /*
- *  Error-checked semaphore signal.
+ *  Error-checked semaphore signal from provided skeleton
  */
 void semaphore_signal(sem_t *sem)
 {
@@ -245,7 +254,7 @@ void semaphore_signal(sem_t *sem)
 }
 
 
-// lightswitch implementation to set the direction of the staircase.
+// Lightswitch implementation to set the direction of the staircase.
 void switch_lock(lightswitch_t *direction, sem_t *direction_semaphore, sem_t *locking_semaphore)
 {
     semaphore_wait(direction_semaphore);
@@ -258,7 +267,7 @@ void switch_lock(lightswitch_t *direction, sem_t *direction_semaphore, sem_t *lo
     semaphore_signal(direction_semaphore);
 }
 
-// lightswitch implementation to unset the staircase direction.
+// lightswitch implementation to reset the staircase direction.
 void switch_unlock(lightswitch_t *direction, sem_t *direction_semaphore, sem_t *locking_semaphore)
 {
     semaphore_wait(direction_semaphore);
@@ -271,6 +280,7 @@ void switch_unlock(lightswitch_t *direction, sem_t *direction_semaphore, sem_t *
     semaphore_signal(direction_semaphore);
 }
 
+// Function to print the turnaround time and response time for each thread.
 void print_clock_array(void)
 {
     printf("************Process Stats**********\n");
